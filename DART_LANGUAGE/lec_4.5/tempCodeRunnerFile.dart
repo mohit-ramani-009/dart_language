e055552297651c24c51b5e54904401eb2ca3fc51
mohit_ramani_009 <158850519+mohit-ramani-009@users.noreply.github.com>
2024-06-11 14:54:47 +0530

    Add files via upload

---
+++ DART_LANGUAGE/lec_4.5/tempCodeRunnerFile.dart
@@ -0,0 +1,18 @@
+void main() 
+{
+  String input = "abc"; 
+
+  try 
+  {
+    int number = int.parse(input);
+    print("The number is: $number");
+  } 
+  catch (FormatException) 
+  {
+    print("FormatException caught! Invalid input format.");
+
+  } finally 
+  {
+    print("thanks for visit...");
+  }
+}
